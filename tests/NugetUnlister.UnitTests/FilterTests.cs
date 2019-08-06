7081740d5c2f557955612cbe723d4e987413094c
Daniel Crenna <daniel.crenna@gmail.com>
2019-08-06 16:45:50 -0400

    add support for ReleaseBefore commands

--- tests/NugetUnlister.UnitTests/FilterTests.cs
+++ tests/NugetUnlister.UnitTests/FilterTests.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,14 +13,28 @@ namespace NugetUnlister.UnitTests
 		[InlineData("0.4.0", "TestContent.input.filterTests.test1.json", "TestContent.output.filterTests.test1.json")]
 		[InlineData("0.4.0.147", "TestContent.input.filterTests.test1.json", "TestContent.output.filterTests.test1.json")]
 		[InlineData("0.4.0-alpha1", "TestContent.input.filterTests.test1.json", "TestContent.output.filterTests.test1.json")]
-		public async Task TestInputs(string semVer, string inputFile, string outputFile)
+		public async Task TestInputs_pre_release(string semVer, string inputFile, string outputFile)
 		{
 			var input = await FileUtility.GetEmbeddedJsonAsync<string[]>(inputFile);
-			var converted = PackageHelper.FilterBefore(new HashSet<string>(input), semVer);
+			var converted = PackageHelper.FilterBefore(new HashSet<string>(input), semVer, true);
 			var output = await FileUtility.GetEmbeddedJsonAsync<string[]>(outputFile);
 			var expected = converted.Select(s => s.version.ToString()).ToArray();
 
 			expected.ShouldBe(output);
 		}
-	}
+
+        [Theory]
+        [InlineData("0.4.0", "TestContent.input.filterTests.test1.json", "TestContent.output.filterTests.test2.json")]
+        [InlineData("0.4.0.147", "TestContent.input.filterTests.test1.json", "TestContent.output.filterTests.test2.json")]
+        [InlineData("0.4.0-alpha1", "TestContent.input.filterTests.test1.json", "TestContent.output.filterTests.test2.json")]
+        public async Task TestInputs_release(string semVer, string inputFile, string outputFile)
+        {
+            var input = await FileUtility.GetEmbeddedJsonAsync<string[]>(inputFile);
+            var converted = PackageHelper.FilterBefore(new HashSet<string>(input), semVer, false);
+            var output = await FileUtility.GetEmbeddedJsonAsync<string[]>(outputFile);
+            var expected = converted.Select(s => s.version.ToString()).ToArray();
+
+            expected.ShouldBe(output);
+        }
+    }
 }
