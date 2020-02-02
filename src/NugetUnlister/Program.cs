6ccc4c5be84f90030cb46625936d4eaf487db495
Andreas Müller <taori@users.noreply.github.com>
2020-02-02 19:15:54 +0100

    Dev/releasefixes (#6)
    * fixes for #4
    
    * semver update + test updates to verify all semver2 compliant variants
    
    * Documentation, reworked debuggability, editorconfig
    
    * dependency updates, improved docs
    
    * formatting

--- src/NugetUnlister/Program.cs
+++ src/NugetUnlister/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
 using CommandDotNet;
+using CommandDotNet.HelpGeneration;
+using CommandDotNet.MicrosoftCommandLineUtils;
+using CommandDotNet.Models;
+using NLog;
 
 namespace NugetUnlister
 {
@@ -7,7 +12,37 @@ namespace NugetUnlister
 	{
 		static int Main(string[] args)
 		{
-			var runner = new AppRunner<ConsoleShell>();
+#if DEBUG
+			if (Debugger.IsAttached)
+			{
+				string input;
+				do
+				{
+					Console.WriteLine("Waiting for user input.");
+					input = Console.ReadLine();
+					if (string.IsNullOrEmpty(input))
+						return 0;
+
+					Console.Clear();
+					var userArgs = input.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+					var code = RunApplication(userArgs);
+					Console.WriteLine(code);
+				} while (input != "exit");
+
+				return 0;
+			}
+			else
+			{
+				return RunApplication(args);
+			}
+#else
+				return RunApplication(args);
+#endif
+		}
+
+		private static int RunApplication(string[] args)
+		{
+			var runner = new AppRunner<ConsoleShell>(CreateAppSettings());
 			try
 			{
 				return runner.Run(args);
@@ -26,6 +61,24 @@ namespace NugetUnlister
 				Console.WriteLine(e.InnerException?.ToString());
 				return int.MinValue;
 			}
+			finally
+			{
+				LogManager.Flush(TimeSpan.FromSeconds(10));
+			}
+		}
+
+		private static AppSettings CreateAppSettings()
+		{
+			var appSettings = new AppSettings();
+			appSettings.MethodArgumentMode = ArgumentMode.Parameter;
+			appSettings.Case = Case.CamelCase;
+			appSettings.Help = new AppHelpSettings()
+			{
+				TextStyle = HelpTextStyle.Detailed,
+				UsageAppNameStyle = UsageAppNameStyle.GlobalTool
+			};
+
+			return appSettings;
 		}
 	}
 }
